57ac071ee342d8bf96308775ab7a6c64b2d085cf
Li <jili1@uiwtx.edu>
2023-11-16 15:39:28 -0600

    added fire abilty and scanner

---
+++ Assets/Prefabs/GameplayAbility/Filre/Fire.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/Fire")]
+public class Fire : Ability
+{
+    [SerializeField] TargetScaner scanerPrefab;
+    [SerializeField] float range = 3;
+    [SerializeField] float scanDuration = 0.5f;
+
+    [SerializeField] float damage = 40f;
+    [SerializeField] float damageDuration = 3f;
+
+    [SerializeField] GameObject ScanVFX;
+    TargetScaner scaner;
+    public override void ActivateAbility()
+    {
+        if (!CommitAbility())
+            return;
+
+        scaner = Instantiate(scanerPrefab);
+        scaner.Init( range, scanDuration, ScanVFX == null ? null : Instantiate(ScanVFX) );
+        scaner.SetupAttachment(OwningAblityComponet.gameObject.transform);
+        scaner.StartScan();
+        scaner.onNewTargetFound += ApplyDamage;
+    }
+
+    private void ApplyDamage(GameObject target)
+    {
+        Debug.Log($"damaging: {target}");
+    }
+}
